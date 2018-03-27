9b609046a6814d21b1472fed5d47000f29f98201
Felipe Lamim <fclamim@gmail.com>
2018-03-27 20:37:57 -0300

    Criando o cliente

--- src/control/Echo.java
+++ src/control/Echo.java
@@ -1,12 +1,13 @@
 package control;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public class Echo {
 	@WebMethod
-	public String Echo(String frase) {
+	public String Echo(@WebParam(name = "frase")String frase) {
 		return "Echo: " + frase;
 	}
 }
---
+++ src/control/Publicador.java
@@ -0,0 +1,9 @@
+package control;
+
+import javax.xml.ws.Endpoint;
+
+public class Publicador {
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:10000/echo", new Echo());
+	}
+}
