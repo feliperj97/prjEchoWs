1f954ea3e331b38b23c64a8f3bc24a68e44c04ae
Felipe Lamim <fclamim@gmail.com>
2018-03-27 20:45:26 -0300

    Criando a classe Programa par ao cliente

---
+++ src/cliente/Programa.java
@@ -0,0 +1,11 @@
+package cliente;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Echo echo = new EchoService().getEchoPort();
+		System.out.println(echo.echo("Alô mundo JAX-WS!"));
+
+	}
+
+}
