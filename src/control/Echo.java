735b7ef60bd1811a1e4445739f0cb071e717c628
Felipe Lamim <richardlemos@unigranrio.br>
2018-03-27 20:04:31 -0300

    fisrt commit

--- src/control/Echo.java
+++ src/control/Echo.java
@@ -1,6 +1,11 @@
 package control;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class Echo {
+	@WebMethod
 	public String Echo(String frase) {
 		return "Echo: " + frase;
 	}
